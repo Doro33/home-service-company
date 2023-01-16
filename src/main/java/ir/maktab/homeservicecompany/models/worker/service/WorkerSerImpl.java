f323ab55bf82f0db351c02bebf19aed3bc363b90
doro <sin.mhm@gmail.com>
2023-01-16 06:36:57 +0330

    6:30 am Sunday

--- src/main/java/ir/maktab/homeservicecompany/models/worker/service/WorkerSerImpl.java
+++ src/main/java/ir/maktab/homeservicecompany/models/worker/service/WorkerSerImpl.java
@@ -2,7 +2,6 @@ package ir.maktab.homeservicecompany.models.worker.service;
 
 import com.google.common.base.Strings;
 import ir.maktab.homeservicecompany.models.job.entity.Job;
-import ir.maktab.homeservicecompany.models.job.service.JobService;
 import ir.maktab.homeservicecompany.models.worker.dto.FilterWorkerDTO;
 import ir.maktab.homeservicecompany.utils.dto.UserDTO;
 import ir.maktab.homeservicecompany.models.worker.entity.WorkerStatus;
@@ -13,7 +12,6 @@ import ir.maktab.homeservicecompany.models.worker.dao.WorkerDao;
 import ir.maktab.homeservicecompany.models.worker.entity.Worker;
 import ir.maktab.homeservicecompany.utils.dto.PasswordDTO;
 import ir.maktab.homeservicecompany.utils.exception.AdminPermitException;
-import ir.maktab.homeservicecompany.utils.exception.NullIdException;
 import ir.maktab.homeservicecompany.utils.validation.Validation;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -33,14 +31,14 @@ public class WorkerSerImpl extends BaseServiceImpl<Worker, WorkerDao> implements
     @PersistenceContext
     private EntityManager em;
 
-    public WorkerSerImpl(WorkerDao repository, JobService jobSer, WorkerSkillService workerSkillSer, Validation validation) {
+    public WorkerSerImpl(WorkerDao repository, WorkerSkillService workerSkillSer, Validation validation) {
         super(repository);
-        this.jobSer = jobSer;
+
         this.workerSkillSer = workerSkillSer;
         this.validation = validation;
     }
 
-    private final JobService jobSer;
+
     private final WorkerSkillService workerSkillSer;
     private final Validation validation;
 
@@ -77,9 +75,7 @@ public class WorkerSerImpl extends BaseServiceImpl<Worker, WorkerDao> implements
 
     @Override
     public void confirmWorker(Long id) {
-        if (id == null)
-            throw new NullIdException("worker id cannot be null.");
-        Worker worker = findById(id);
+        Worker worker = validation.workerValidate(id);
         if (worker.getStatus() == WorkerStatus.SUSPENDED)
             throw new AdminPermitException("worker has been suspended.");
         if (worker.getStatus() == WorkerStatus.CONFIRMED)
@@ -103,13 +99,8 @@ public class WorkerSerImpl extends BaseServiceImpl<Worker, WorkerDao> implements
 
     @Override
     public void addSkill(Long workerId, Long jobId) {
-        if (workerId == null)
-            throw new NullIdException("worker id cannot be null.");
-        if (jobId == null)
-            throw new NullIdException("job id cannot be null.");
-
-        Worker worker = findById(workerId);
-        Job job = jobSer.findById(jobId);
+        Worker worker = validation.workerValidate(workerId);
+        Job job = validation.jobValidate(jobId);
         if (worker.getStatus() != WorkerStatus.CONFIRMED)
             throw new IllegalArgumentException("worker has not been confirmed.");
 
