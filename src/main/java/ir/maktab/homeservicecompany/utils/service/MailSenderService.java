6db1425fc66ae6857966f3a43f1b6d341eeca5a4
doro <sin.mhm@gmail.com>
2023-01-18 09:20:22 +0330

    8:00 am wednesday

--- src/main/java/ir/maktab/homeservicecompany/utils/service/MailSenderService.java
+++ src/main/java/ir/maktab/homeservicecompany/utils/service/MailSenderService.java
@@ -1,15 +1,25 @@
 package ir.maktab.homeservicecompany.utils.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MailSenderService {
-    @Autowired
-    private JavaMailSender mailSender;
 
-    public void sendEmail(String toEmail, String subject, String text){
-        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-        simpleMailMessage.
+    private final JavaMailSender mailSender;
+
+    public MailSenderService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendEmail(String toEmail,String toUserType ,Long id){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("sin.mhm@gmail.com");
+        message.setTo(toEmail);
+        message.setSubject("account activation");
+        message.setText("localhost:8081/" + toUserType +"/activateAccount/"+id);
+
+        mailSender.send(message);
     }
 }
